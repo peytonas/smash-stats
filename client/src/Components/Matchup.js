4c4f96482f36f697ac20fb3d8f537b9fb2fdbf28
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2020-04-13 12:43:25 -0600

    stats change color

--- client/src/Components/Matchup.js
+++ client/src/Components/Matchup.js
@@ -36,34 +36,34 @@ class Matchup extends Component {
               <p className={player.spotDodgeStart >= opponent.spotDodgeStart ? "text-green" : "text-danger"}>{player.spotDodgeStart}</p>
               <p className={player.spotDodgeLag >= opponent.spotDodgeLag ? "text-green" : "text-danger"}>{player.spotDodgeLag}</p>
               <p className={player.airDodgeLag >= opponent.airDodgeLag ? "text-green" : "text-danger"}>{player.airDodgeLag}</p>
-              <p>{player.neutralSpecial}</p>
-              <p>{player.sideSpecial}</p>
-              <p>{player.upSpecial}</p>
-              <p>{player.downSpecial}</p>
-              <p>{player.neutral}</p>
-              <p>{player.upTilt}</p>
-              <p>{player.downTilt}</p>
-              <p>{player.forwardTilt}</p>
-              <p>{player.dashAttack}</p>
-              <p>{player.upSmash}</p>
-              <p>{player.downSmash}</p>
-              <p>{player.forwardSmash}</p>
-              <p>{player.nair}</p>
-              <p>{player.uair}</p>
-              <p>{player.dair}</p>
-              <p>{player.fair}</p>
-              <p>{player.bair}</p>
-              <p>{player.zair}</p>
-              <p>{player.pummel}</p>
-              <p>{player.forwardThrow}</p>
-              <p>{player.backThrow}</p>
-              <p>{player.upThrow}</p>
-              <p>{player.downThrow}</p>
-              <p>{player.floorAttackFront}</p>
-              <p>{player.floorAttackBack}</p>
-              <p>{player.floorAttackTrip}</p>
-              <p>{player.edgeAttack}</p>
-              <p>{player.finalSmash}</p>
+              <p className={player.neutralSpecial >= opponent.neutralSpecial ? "text-green" : "text-danger"}>{player.neutralSpecial}</p>
+              <p className={player.sideSpecial >= opponent.sideSpecial ? "text-green" : "text-danger"}>{player.sideSpecial}</p>
+              <p className={player.upSpecial >= opponent.upSpecial ? "text-green" : "text-danger"}>{player.upSpecial}</p>
+              <p className={player.downSpecial >= opponent.downSpecial ? "text-green" : "text-danger"}>{player.downSpecial}</p>
+              <p className={player.neutral >= opponent.neutral ? "text-green" : "text-danger"}>{player.neutral}</p>
+              <p className={player.upTilt >= opponent.upTilt ? "text-green" : "text-danger"}>{player.upTilt}</p>
+              <p className={player.downTilt >= opponent.downTilt ? "text-green" : "text-danger"}>{player.downTilt}</p>
+              <p className={player.forwardTilt >= opponent.forwardTilt ? "text-green" : "text-danger"}>{player.forwardTilt}</p>
+              <p className={player.dashAttack >= opponent.dashAttack ? "text-green" : "text-danger"}>{player.dashAttack}</p>
+              <p className={player.upSmash >= opponent.upSmash ? "text-green" : "text-danger"}>{player.upSmash}</p>
+              <p className={player.downSmash >= opponent.downSmash ? "text-green" : "text-danger"}>{player.downSmash}</p>
+              <p className={player.forwardSmash >= opponent.forwardSmash ? "text-green" : "text-danger"}>{player.forwardSmash}</p>
+              <p className={player.nair >= opponent.nair ? "text-green" : "text-danger"}>{player.nair}</p>
+              <p className={player.uair >= opponent.uair ? "text-green" : "text-danger"}>{player.uair}</p>
+              <p className={player.dair >= opponent.dair ? "text-green" : "text-danger"}>{player.dair}</p>
+              <p className={player.fair >= opponent.fair ? "text-green" : "text-danger"}>{player.fair}</p>
+              <p className={player.bair >= opponent.bair ? "text-green" : "text-danger"}>{player.bair}</p>
+              <p className={player.zair >= opponent.zair ? "text-green" : "text-danger"}>{player.zair}</p>
+              <p className={player.pummel >= opponent.pummel ? "text-green" : "text-danger"}>{player.pummel}</p>
+              <p className={player.forwardThrow >= opponent.forwardThrow ? "text-green" : "text-danger"}>{player.forwardThrow}</p>
+              <p className={player.backThrow >= opponent.backThrow ? "text-green" : "text-danger"}>{player.backThrow}</p>
+              <p className={player.upThrow >= opponent.upThrow ? "text-green" : "text-danger"}>{player.upThrow}</p>
+              <p className={player.downThrow >= opponent.downThrow ? "text-green" : "text-danger"}>{player.downThrow}</p>
+              <p className={player.floorAttackFront >= opponent.floorAttackFront ? "text-green" : "text-danger"}>{player.floorAttackFront}</p>
+              <p className={player.floorAttackBack >= opponent.floorAttackBack ? "text-green" : "text-danger"}>{player.floorAttackBack}</p>
+              <p className={player.floorAttackTrip >= opponent.floorAttackTrip ? "text-green" : "text-danger"}>{player.floorAttackTrip}</p>
+              <p className={player.edgeAttack >= opponent.edgeAttack ? "text-green" : "text-danger"}>{player.edgeAttack}</p>
+              <p className={player.finalSmash >= opponent.finalSmash ? "text-green" : "text-danger"}>{player.finalSmash}</p>
             </div>
           </div>
           <div className="col-3 text-center">
@@ -124,34 +124,34 @@ class Matchup extends Component {
               <p className={opponent.spotDodgeStart >= player.spotDodgeStart ? "text-green" : "text-danger"}>{opponent.spotDodgeStart}</p>
               <p className={opponent.spotDodgeLag >= player.spotDodgeLag ? "text-green" : "text-danger"}>{opponent.spotDodgeLag}</p>
               <p className={opponent.airDodgeLag >= player.airDodgeLag ? "text-green" : "text-danger"}>{opponent.airDodgeLag}</p>
-              <p>{opponent.neutralSpecial}</p>
-              <p>{opponent.sideSpecial}</p>
-              <p>{opponent.upSpecial}</p>
-              <p>{opponent.downSpecial}</p>
-              <p>{opponent.neutral}</p>
-              <p>{opponent.upTilt}</p>
-              <p>{opponent.downTilt}</p>
-              <p>{opponent.forwardTilt}</p>
-              <p>{opponent.dashAttack}</p>
-              <p>{opponent.upSmash}</p>
-              <p>{opponent.downSmash}</p>
-              <p>{opponent.forwardSmash}</p>
-              <p>{opponent.nair}</p>
-              <p>{opponent.uair}</p>
-              <p>{opponent.dair}</p>
-              <p>{opponent.fair}</p>
-              <p>{opponent.bair}</p>
-              <p>{opponent.zair}</p>
-              <p>{opponent.pummel}</p>
-              <p>{opponent.forwardThrow}</p>
-              <p>{opponent.backThrow}</p>
-              <p>{opponent.upThrow}</p>
-              <p>{opponent.downThrow}</p>
-              <p>{opponent.floorAttackFront}</p>
-              <p>{opponent.floorAttackBack}</p>
-              <p>{opponent.floorAttackTrip}</p>
-              <p>{opponent.edgeAttack}</p>
-              <p>{opponent.finalSmash}</p>
+              <p className={opponent.neutralSpecial >= player.neutralSpecial ? "text-green" : "text-danger"}>{opponent.neutralSpecial}</p>
+              <p className={opponent.sideSpecial >= player.sideSpecial ? "text-green" : "text-danger"}>{opponent.sideSpecial}</p>
+              <p className={opponent.upSpecial >= player.upSpecial ? "text-green" : "text-danger"}>{opponent.upSpecial}</p>
+              <p className={opponent.downSpecial >= player.downSpecial ? "text-green" : "text-danger"}>{opponent.downSpecial}</p>
+              <p className={opponent.neutral >= player.neutral ? "text-green" : "text-danger"}>{opponent.neutral}</p>
+              <p className={opponent.upTilt >= player.upTilt ? "text-green" : "text-danger"}>{opponent.upTilt}</p>
+              <p className={opponent.downTilt >= player.downTilt ? "text-green" : "text-danger"}>{opponent.downTilt}</p>
+              <p className={opponent.forwardTilt >= player.forwardTilt ? "text-green" : "text-danger"}>{opponent.forwardTilt}</p>
+              <p className={opponent.dashAttack >= player.dashAttack ? "text-green" : "text-danger"}>{opponent.dashAttack}</p>
+              <p className={opponent.upSmash >= player.upSmash ? "text-green" : "text-danger"}>{opponent.upSmash}</p>
+              <p className={opponent.downSmash >= player.downSmash ? "text-green" : "text-danger"}>{opponent.downSmash}</p>
+              <p className={opponent.forwardSmash >= player.forwardSmash ? "text-green" : "text-danger"}>{opponent.forwardSmash}</p>
+              <p className={opponent.nair >= player.nair ? "text-green" : "text-danger"}>{opponent.nair}</p>
+              <p className={opponent.uair >= player.uair ? "text-green" : "text-danger"}>{opponent.uair}</p>
+              <p className={opponent.dair >= player.dair ? "text-green" : "text-danger"}>{opponent.dair}</p>
+              <p className={opponent.fair >= player.fair ? "text-green" : "text-danger"}>{opponent.fair}</p>
+              <p className={opponent.bair >= player.bair ? "text-green" : "text-danger"}>{opponent.bair}</p>
+              <p className={opponent.zair >= player.zair ? "text-green" : "text-danger"}>{opponent.zair}</p>
+              <p className={opponent.pummel >= player.pummel ? "text-green" : "text-danger"}>{opponent.pummel}</p>
+              <p className={opponent.forwardThrow >= player.forwardThrow ? "text-green" : "text-danger"}>{opponent.forwardThrow}</p>
+              <p className={opponent.backThrow >= player.backThrow ? "text-green" : "text-danger"}>{opponent.backThrow}</p>
+              <p className={opponent.upThrow >= player.upThrow ? "text-green" : "text-danger"}>{opponent.upThrow}</p>
+              <p className={opponent.downThrow >= player.downThrow ? "text-green" : "text-danger"}>{opponent.downThrow}</p>
+              <p className={opponent.floorAttackFront >= player.floorAttackFront ? "text-green" : "text-danger"}>{opponent.floorAttackFront}</p>
+              <p className={opponent.floorAttackBack >= player.floorAttackBack ? "text-green" : "text-danger"}>{opponent.floorAttackBack}</p>
+              <p className={opponent.floorAttackTrip >= player.floorAttackTrip ? "text-green" : "text-danger"}>{opponent.floorAttackTrip}</p>
+              <p className={opponent.edgeAttack >= player.edgeAttack ? "text-green" : "text-danger"}>{opponent.edgeAttack}</p>
+              <p className={opponent.finalSmash >= player.finalSmash ? "text-green" : "text-danger"}>{opponent.finalSmash}</p>
             </div>
           </div>
         </div>
