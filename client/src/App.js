1e8b40d20b5649ce39dcfc3de392605e7ab99ecc
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2020-04-14 10:59:56 -0600

    api tweaks

--- client/src/App.js
+++ client/src/App.js
@@ -15,11 +15,17 @@ class App extends Component {
   }
 
   selectedNames = (playerId, opponentId) => {
-    Axios.get(`http://localhost:3000/api/characters/${playerId}`)
+    let base = window.location.host.includes('localhost:8080') ? '//localhost:3000/' : '/'
+    let api = Axios.create({
+      baseURL: base + 'api/',
+      timeout: 3000,
+      withCredentials: true
+    })
+    api.get(`http://localhost:3000/api/characters/${playerId}`)
       .then(res => {
         this.setState({ playerChoice: res.data });
       })
-    Axios.get(`http://localhost:3000/api/characters/${opponentId}`)
+    api.get(`http://localhost:3000/api/characters/${opponentId}`)
       .then(res => {
         this.setState({ opponentChoice: res.data });
       })
