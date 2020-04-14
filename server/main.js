f775186bda41ccac089a172e8f01741137aa84f9
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2020-04-14 12:15:47 -0600

    nothing

--- server/main.js
+++ server/main.js
@@ -39,13 +39,4 @@ server.use('*', (req, res, next) => {
   res.status(404).send("Route not found")
 })
 
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  server.use(express.static(path.join(__dirname, 'client/build')));
-  // Handle React routing, return all requests to React app
-  server.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
-
 server.listen(port, () => { console.log(`Server is running on port: ${port}`) })
