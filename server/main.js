ed45424a88161b201e03411dbf08233e19988071
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2020-04-20 13:26:39 -0600

    fix

--- server/main.js
+++ server/main.js
@@ -1,37 +1,23 @@
 const express = require('express')
 const cors = require('cors')
 const bp = require('body-parser')
-const compression = require('compression')
-const morgan = require('morgan')
 const path = require('path')
 import DbConfig from "./db/DbConfig"
 
-const normalizePort = port => parseInt(port, 10)
-const port = normalizePort(process.env.PORT || 3000)
+
+const port = process.env.PORT || 3000
 
 let server = express()
 const dev = server.get('env') !== 'production'
 
 DbConfig.connect()
 
-if (!dev) {
-  server.disable('x-powered-by')
-  // @ts-ignore
-  server.use(compression())
-  // @ts-ignore
-  server.use(morgan('common'))
-  server.use(express.static('client/build'))
-  server.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-  })
-}
+server.use(express.static('client/build'))
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+})
 
-if (dev) {
-  // @ts-ignore
-  server.use(morgan('dev'))
-}
-
-let whitelist = ['http://localhost:8080', 'https://ssb-stats.herokuapp.com'];
+let whitelist = ['http://localhost:8080', 'https://fierce-garden-50211.herokuapp.com'];
 let corsOptions = {
   origin: function (origin, callback) {
     let originIsWhitelisted = whitelist.indexOf(origin) !== -1;
