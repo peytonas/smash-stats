f3021d49b5b0a1790cd3f1461db8345dee038f4f
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2020-04-27 18:00:30 -0600

    moved build folder

---
+++ server/build/precache-manifest.32fcd8288e423f7154cee183dfc04f68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "943b1b5a0e5dfc7ccc58fd954a881e72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e432d85be2bf8c93ddde",
+    "url": "/static/css/main.5e9e34cd.chunk.css"
+  },
+  {
+    "revision": "72bf775b5d846efc6b14",
+    "url": "/static/js/2.bf8024f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bf8024f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e432d85be2bf8c93ddde",
+    "url": "/static/js/main.e4f4e0db.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  }
+]);
