a861bd2fd61d13726057a429594e3a93822668ba
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2020-06-24 11:35:46 -0600

    counter build

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,36 +1,36 @@
 import React, { Component } from 'react';
-// import Counter from '../Components/Counter.js'
+import Counter from '../Components/Counter.js'
 import Axios from 'axios';
 
-// let base = window.location.host.includes('localhost:8080' || 'https://ssb-stats.herokuapp.com') ? '//localhost:3000/' : '/'
-// let api = Axios.create({
-//   baseURL: base + 'api/',
-//   timeout: 6000,
-//   withCredentials: true
-// })
+let base = window.location.host.includes('localhost:8080' || 'https://ssb-stats.herokuapp.com') ? '//localhost:3000/' : '/'
+let api = Axios.create({
+  baseURL: base + 'api/',
+  timeout: 6000,
+  withCredentials: true
+})
 
 class Footer extends Component {
 
-  // state = {
-  //   hitsObject: {}
-  // }
+  state = {
+    hitsObject: {}
+  }
 
-  // getHits() {
-  //   api.get(`/hits`)
-  //     .then(res => {
-  //       this.setState({ hitsObject: res.data[0] })
-  //       this.editHits()
-  //     })
-  // }
+  getHits() {
+    api.get(`/hits`)
+      .then(res => {
+        this.setState({ hitsObject: res.data[0] })
+        this.editHits()
+      })
+  }
 
-  // editHits() {
-  //   this.setState({ hitsObject: { _id: this.state.hitsObject._id, hits: this.state.hitsObject.hits + 1 } })
-  //   api.put(`/hits/${this.state.hitsObject._id}`, this.state.hitsObject)
-  // }
+  editHits() {
+    this.setState({ hitsObject: { _id: this.state.hitsObject._id, hits: this.state.hitsObject.hits + 1 } })
+    api.put(`/hits/${this.state.hitsObject._id}`, this.state.hitsObject)
+  }
 
-  // componentDidMount() {
-  //   this.getHits()
-  // }
+  componentDidMount() {
+    this.getHits()
+  }
 
   render() {
     return (
@@ -40,7 +40,7 @@ class Footer extends Component {
           <a href="https://www.youtube.com/channel/UCkQmM3FZJXlOo3cf2n-MwEQ" target="_blank" rel="noopener noreferrer"><i className="fa fa-youtube ml-2 text-success"></i></a>
           <a href="https://venmo.com/Peyton-Sonnefeld" target="_blank" rel="noopener noreferrer"><i className="fa fa-heartbeat ml-2 text-success"></i></a>
         </div>
-        {/* <Counter hits={this.state.hitsObject.hits} /> */}
+        <Counter hits={this.state.hitsObject.hits} />
       </footer>
     )
   }
---
+++ server/build/precache-manifest.13ef0a1771bdccffb6ea3d5e02dd1ed7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a776bffa8b6729b0f0e00f95b47e112",
+    "url": "/index.html"
+  },
+  {
+    "revision": "502b2d0a7a0112d6a59f",
+    "url": "/static/css/main.7fe455dd.chunk.css"
+  },
+  {
+    "revision": "05d737f7ebda5d022e11",
+    "url": "/static/js/2.79ab1fba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.79ab1fba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "502b2d0a7a0112d6a59f",
+    "url": "/static/js/main.133e4681.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  }
+]);
