0bb6f0666e229898c555453aad27bab87b6ca828
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2020-06-24 11:31:51 -0600

    test rebuild

--- client/src/Components/Counter.js
+++ client/src/Components/Counter.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types'
+// import React, { Component } from 'react';
+// import PropTypes from 'prop-types'
 
-class Counter extends Component {
-  render() {
-    return (
-      <div className="col col-md-3 text-right mr-1">
-        00000{this.props.hits}
-      </div >
-    )
-  }
-}
+// class Counter extends Component {
+//   render() {
+//     return (
+//       <div className="col col-md-3 text-right mr-1">
+//         00000{this.props.hits}
+//       </div >
+//     )
+//   }
+// }
 
-Counter.propTypes = {
-  hits: PropTypes.number.isRequired
-}
+// Counter.propTypes = {
+//   hits: PropTypes.number.isRequired
+// }
 
-export default Counter
+// export default Counter
