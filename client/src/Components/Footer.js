22199a9051d459e58b808b7e4b71c54d66457000
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2020-06-24 11:20:25 -0600

    hit counter works

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -2,25 +2,35 @@ import React, { Component } from 'react';
 import Counter from '../Components/Counter.js'
 import Axios from 'axios';
 
+let base = window.location.host.includes('localhost:8080' || 'https://ssb-stats.herokuapp.com') ? '//localhost:3000/' : '/'
+let api = Axios.create({
+  baseURL: base + 'api/',
+  timeout: 3000,
+  withCredentials: true
+})
+
 class Footer extends Component {
 
   state = {
-    hitsId: "",
-    hits: 0
+    hitsObject: {}
+  }
+
+  getHits() {
+    api.get(`/hits`)
+      .then(res => {
+        this.setState({ hitsObject: res.data[0] })
+        this.editHits()
+      })
+  }
+
+  editHits() {
+    this.setState({ hitsObject: { _id: this.state.hitsObject._id, hits: this.state.hitsObject.hits + 1 } })
+    console.log(this.state.hitsObject.hits);
+    api.put(`/hits/${this.state.hitsObject._id}`, this.state.hitsObject)
   }
 
   componentDidMount() {
-    let base = window.location.host.includes('localhost:8080' || 'https://ssb-stats.herokuapp.com') ? '//localhost:3000/' : '/'
-    let api = Axios.create({
-      baseURL: base + 'api/',
-      timeout: 3000,
-      withCredentials: true
-    })
-    api.get(`/hits`)
-      .then(res => {
-        this.setState({ hitsId: res.data[0]._id })
-        this.setState({ hits: res.data[0].hits })
-      })
+    this.getHits()
   }
 
   render() {
@@ -31,7 +41,7 @@ class Footer extends Component {
           <a href="https://www.youtube.com/channel/UCkQmM3FZJXlOo3cf2n-MwEQ" target="_blank" rel="noopener noreferrer"><i className="fa fa-youtube ml-2 text-success"></i></a>
           <a href="https://venmo.com/Peyton-Sonnefeld" target="_blank" rel="noopener noreferrer"><i className="fa fa-heartbeat ml-2 text-success"></i></a>
         </div>
-        <Counter hits={this.state.hits} />
+        <Counter hits={this.state.hitsObject.hits} />
       </footer>
     )
   }
